315e10e369b5285ef86422b775b27c8b24e45246
miau1 <aulamo.mikko@gmail.com>
2018-07-11 12:31:32 +0300

    added downloading functionality

--- opus_get.py
+++ opus_get.py
@@ -1,4 +1,4 @@
-import urllib.request, json, argparse
+import urllib.request, json, argparse, sys
 
 class OpusGet:
 
@@ -10,6 +10,7 @@ class OpusGet:
         parser.add_argument("-r", help="Release")
         parser.add_argument("-p", help="Pre-process type")
         parser.add_argument("-l", help="List resources", action="store_true")
+        parser.add_argument("-dl", help="Set download directory (default=current directory)", default="")
 
         if len(arguments) == 0:
             self.args = parser.parse_args()
@@ -26,22 +27,28 @@ class OpusGet:
                 else:
                     self.url += urlparts[a] + "=" + self.args.__dict__[a] + "&"
 
+    def round_size(self, size, length, unit):
+        last_n = str(size)[-length]
+        size = int(str(size)[:-length])
+        if int(last_n) >= 5:
+            size += 1
+        size = str(size) + unit
+        return size
         
     def format_size(self, size):
         if len(str(size)) > 9:
-            size = str(size)[:-9] + " TB"
+            size = self.round_size(size, 9, " TB")
         elif len(str(size)) > 6:
-            size = str(size)[:-6] + " GB"
+            size = self.round_size(size, 6, " GB")
         elif len(str(size)) > 3:
-            size = str(size)[:-3] + " MB"
+            size = self.round_size(size, 3, " MB")
         else:
-            size = str(size) + " B"
+            size = str(size) + " KB"
         return(size)
 
 
     def get_response(self, url):
         response = urllib.request.urlopen(url[:-1])
-
         data = json.loads(response.read())
 
         return data
@@ -68,14 +75,14 @@ class OpusGet:
 
         retdata = self.add_data_with_aligment(tempdata, retdata)
         return retdata
-            
-    def print_results(self):
+
+    def get_corpora_data(self):
         file_n = 0
         total_size = 0
-        
-        data = self.get_response(self.url)
 
-        if self.args.t:
+        data = self.get_response(self.url)
+        
+        if self.args.t and not self.args.p:
             corpora = self.remove_data_with_no_alignment(data)
         else:
             corpora = data["corpora"]
@@ -86,14 +93,42 @@ class OpusGet:
 
         total_size = self.format_size(total_size)
 
-        if not self.args.l:
-            answer = input("Downloading " + str(file_n) + " file(s) with total size of " + total_size + ". Continue? (y/n) ")
+        return corpora, file_n, total_size
 
-            if answer == "y":
-                for c in corpora:
-                    print("{0:>7} {1:s}".format(self.format_size(c["size"]), c["url"]))
-        else:
+    def progress_status(self, count, blockSize, totalSize):
+        percent = int(count*blockSize*100/totalSize)
+        print("\r{0} ... {1}% of {2}".format(self.filename, percent, self.filesize), end="", flush=True)
+    
+    def download(self, corpora, file_n, total_size):
+        answer = input("Downloading " + str(file_n) + " file(s) with the total size of " + total_size + ". Continue? (y/n) ")
+            
+        if answer == "y":
             for c in corpora:
-                print("{0:>7} {1:s}".format(self.format_size(c["size"]), c["url"]))
-            print("\n{0:>7} {1:s}".format(total_size, "total size"))
+                self.filename = c["url"].replace("/", "_")
+                self.filesize = self.format_size(c["size"])
+                try:
+                    urllib.request.urlretrieve("https://object.pouta.csc.fi/OPUS/" + c["url"],
+                                               self.args.dl + self.filename, reporthook=self.progress_status)
+                    print("")
+                except urllib.error.URLError as e:
+                    print("Unable the retrieve the data.")
+                    return
+
+    def print_files(self, corpora, file_n, total_size):
+        for c in corpora:
+            print("{0:>7} {1:s}".format(self.format_size(c["size"]), c["url"]))
+        print("\n{0:>7} {1:s}".format(total_size, "Total size"))
+
+    
+    def get_files(self):
+        try:
+            corpora, file_n, total_size = self.get_corpora_data()
+        except urllib.error.URLError as e:
+            print("Unable to retrieve the data.")
+            return
+
+        if not self.args.l:
+            self.download(corpora, file_n, total_size)
+        else:
+            self.print_files(corpora, file_n, total_size)
 
