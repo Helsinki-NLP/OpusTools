fc684b47e658d4f56d32ee15b35691b6a474d5c5
miau1 <aulamo.mikko@gmail.com>
2019-01-25 12:22:20 +0200

    added opus_get script

--- opustools_pkg/setup.py
+++ opustools_pkg/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="opustools_pkg",
-    version="0.0.15",
+    version="0.0.16",
     author="Mikko Aulamo",
     author_email="mikko.aulamo@helsinki.fi",
     description="Tools to read OPUS",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     #url="https://github.com/pypa/example-project",
     packages=setuptools.find_packages(),
-    scripts=["bin/opus_read", "bin/opus_cat"],
+    scripts=["bin/opus_read", "bin/opus_cat", "bin/opus_get"],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
