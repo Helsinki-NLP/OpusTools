403cdb2e20c69c564df9d30929cdecbf2d334381
miau1 <aulamo.mikko@gmail.com>
2019-08-01 11:44:37 +0300

    update version

--- opustools_pkg/setup.py
+++ opustools_pkg/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="opustools_pkg",
-    version="0.0.38",
+    version="0.0.39",
     author="Mikko Aulamo",
     author_email="mikko.aulamo@helsinki.fi",
     description="Tools to read OPUS",
