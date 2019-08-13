1c24a818883b1a535d59e810f5f7ca924a63047d
miau1 <aulamo.mikko@gmail.com>
2019-08-13 11:30:06 +0300

    add subword-nmt

--- opustools_pkg/opustools_pkg/opus_filter.sh
+++ opustools_pkg/opustools_pkg/opus_filter.sh
@@ -13,3 +13,9 @@ python check_clean.py $slan $tlan
 #python ~/appl_taito/eflomal/makepriors.py -i pairs.${slan}-${tlan} -f ${slan}-${tlan}.fwd -r ${slan}-${tlan}.rev --priors ${slan}-${tlan}.priors
 #python ~/appl_taito/eflomal/align.py -i pairs.${slan}-${tlan}.small --priors ${slan}-${tlan}.priors --model 3 -f ${slan}-${tlan}.small.fwd -r ${slan}-${tlan}.small.rev
 python ~/appl_taito/eflomal/align.py -i pairs.${slan}-${tlan} -m 3 -M 3 -F ${slan}-${tlan}.fwd -R ${slan}-${tlan}.rev
+
+subword-nmt learn-bpe -s 37000 < pairs.${slan} > ${slan}.bpe
+subword-nmt apply-bpe -c ${slan}.bpe < pairs.${slan} > pairs.seg.${slan}
+
+subword-nmt learn-bpe -s 37000 < pairs.${tlan} > ${tlan}.bpe
+subword-nmt apply-bpe -c ${tlan}.bpe < pairs.${tlan} > pairs.seg.${tlan}
