435abb5aaa5d37fa39baf14c46ef10e63a024e52
miau1 <aulamo.mikko@gmail.com>
2019-08-19 17:09:51 +0300

    varikn and clean-corpus-n in python

--- opustools_pkg/opus_filter.py
+++ opustools_pkg/opus_filter.py
@@ -1,14 +1,17 @@
+import os
 import argparse
 import gzip
 import math
 import difflib
 import string
 import subprocess
+import math
 
 from opustools_pkg.parse.alignment_parser import AlignmentParser
 
+import varikn
 from bs4 import BeautifulSoup as bs
-import pycld2
+#import pycld2
 #from mosestokenizer import MosesTokenizer
 #import eflomal
 from langid.langid import LanguageIdentifier, model
@@ -45,6 +48,7 @@ args.pa = False
 args.sa, args.ta = '', ''
 args.ca = '\t'
 args.a = 'any'
+args.tr = None
 
 fromto = sorted([args.s, args.t])
 
@@ -54,6 +58,11 @@ alignment = (
     root_dir+args.d+'/'+args.r+'/xml/'+fromto[0]+'-'+fromto[1]+'.xml.gz')
 source = (root_dir+args.d+'/'+args.r+'/'+args.p+'/'+fromto[0]+'.zip')
 target = (root_dir+args.d+'/'+args.r+'/'+args.p+'/'+fromto[1]+'.zip')
+
+try:
+    os.mkdir('filter_files')
+except FileExistsError:
+    pass
 
 def largeRatio(len1, len2):
     lens = sorted([len1, len2])
@@ -91,26 +100,27 @@ def cleanPair(sent1, sent2, script1, script2):
 
 
 def detectLanguage(sentence, lan):
-    try:
-        clddetails = pycld2.detect(sentence)
-    except Exception as e:
-        clddetails = (0, 0, ((0, 'un', 0.0), 0))
+    #try:
+    #    clddetails = pycld2.detect(sentence)
+    #except Exception as e:
+    #    clddetails = (0, 0, ((0, 'un', 0.0), 0))
     try:
         lidetails = identifier.classify(sentence)
     except Exception as e:
         lidetails = ('un', 0.0)
 
-    cldlan = clddetails[2][0][1]
-    cldconf = str(round(clddetails[2][0][2]/100, 2))
+    #cldlan = clddetails[2][0][1]
+    #cldconf = str(round(clddetails[2][0][2]/100, 2))
     lilan, liconf = [str(round(x,2)) if type(x) == float
             else x for x in lidetails]
 
-    if cldlan != lan:
-        cldconf = 0.0
+    #if cldlan != lan:
+    #    cldconf = 0.0
     if lilan != lan:
         liconf = 0.0
 
-    return cldlan, cldconf, lilan, liconf
+    #return lilan, liconf, cldlan, cldconf
+    return lilan, liconf
 
 def getTPunct(line):
     pun = len([c for c in line if c in ['.', '?', '!']])
@@ -157,10 +167,12 @@ def characterScore(line, script):
 alignment_parser = AlignmentParser(
     source, target, args, '', '', '', fromto, False)
 
-source_file = open('sents.{}'.format(fromto[0]), 'w')
-target_file = open('sents.{}'.format(fromto[1]), 'w')
-score_file = open('scores.{0}-{1}'.format(fromto[0], fromto[1]), 'w')
-clean_file = open('clean.{0}-{1}'.format(fromto[0], fromto[1]), 'w')
+source_file = open('filter_files/sents.{}'.format(fromto[0]), 'w')
+target_file = open('filter_files/sents.{}'.format(fromto[1]), 'w')
+score_file = open('filter_files/scores.{0}-{1}'.format(fromto[0], fromto[1])
+                 , 'w')
+clean_file = open('filter_files/clean.{0}-{1}'.format(fromto[0], fromto[1])
+                 , 'w')
 
 #s_tokenizer = MosesTokenizer(fromtto[0])
 #t_tokenizer = MosesTokenizer(fromtto[1])
@@ -183,7 +195,7 @@ with gzip.open(alignment) as gzipAlign:
             terPun = terminalPunctuation(ssent, tsent)
             noZeNu = nonZeroNumerals(ssent, tsent)
             score_file.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n'.format(
-                slang[3], tlang[3], schars, tchars,
+                slang[1], tlang[1], schars, tchars,
                 terPun, noZeNu))
 
 source_file.close()
@@ -192,22 +204,107 @@ score_file.close()
 clean_file.close()
 
 eflomal_path = '/homeappl/home/miaulamo/appl_taito/eflomal/'
-subprocess.run('{0}align.py -i clean.{1}-{2} -f {1}-{2}.fwd -r '
-    '{1}-{2}.rev --model 3'.format(
-        eflomal_path, fromto[0], fromto[1]).split())
-subprocess.run('{0}makepriors.py -i clean.{1}-{2} -f {1}-{2}.fwd -r '
-    '{1}-{2}.rev --priors {1}-{2}.priors'.format(
-        eflomal_path, fromto[0], fromto[1]).split())
-subprocess.run('{0}align.py -s sents.{1} -t sents.{2} -F {1}-{2}_score.fwd -R '
-    '{1}-{2}_score.rev --model 3 -M 3 --priors {1}-{2}.priors'.format(
-        eflomal_path, fromto[0], fromto[1]).split())
+subprocess.run('{0}align.py -i filter_files/clean.{1}-{2} -f '
+        'filter_files/{1}-{2}.fwd -r filter_files/{1}-{2}.rev '
+        '--model 3'.format(
+            eflomal_path, fromto[0], fromto[1]).split())
+subprocess.run('{0}makepriors.py -i filter_files/clean.{1}-{2} -f ' 
+        'filter_files/{1}-{2}.fwd -r filter_files/{1}-{2}.rev --priors '
+        'filter_files/{1}-{2}.priors'.format(
+            eflomal_path, fromto[0], fromto[1]).split())
+subprocess.run('{0}align.py -s filter_files/sents.{1} -t '
+        'filter_files/sents.{2} -F filter_files/{1}-{2}_score.fwd -R '
+        'filter_files/{1}-{2}_score.rev --model 3 -M 3 --priors '
+        'filter_files/{1}-{2}.priors'.format(
+            eflomal_path, fromto[0], fromto[1]).split())
 
-subprocess.run('subword-nmt learn-bpe -s 37000 < sents.{0} > '
-    '{0}.bpe'.format(fromto[0]), shell=True)
-subprocess.run('subword-nmt apply-bpe -c {0}.bpe < sents.{0} > '
-    'sents.seg.{0}'.format(fromto[0]), shell=True)
+subprocess.run('subword-nmt learn-bpe -s 37000 < filter_files/sents.{0} > '
+    'filter_files/{0}.bpe'.format(fromto[0]), shell=True)
+subprocess.run('subword-nmt apply-bpe -c filter_files/{0}.bpe < filter_files/sents.{0} > '
+    'filter_files/sents.bpe.{0}'.format(fromto[0]), shell=True)
 
-subprocess.run('subword-nmt learn-bpe -s 37000 < sents.{0} > '
-    '{0}.bpe'.format(fromto[1]), shell=True)
-subprocess.run('subword-nmt apply-bpe -c {0}.bpe < sents.{0} > '
-    'sents.seg.{0}'.format(fromto[1]), shell=True)
+subprocess.run('subword-nmt learn-bpe -s 37000 < filter_files/sents.{0} > '
+    'filter_files/{0}.bpe'.format(fromto[1]), shell=True)
+subprocess.run('subword-nmt apply-bpe -c filter_files/{0}.bpe < filter_files/sents.{0} > '
+    'filter_files/sents.bpe.{0}'.format(fromto[1]), shell=True)
+
+def segment(lang):
+    with open('filter_files/sents.bpe.{}'.format(lang)) as infile:
+        with open('filter_files/sents.seg.{}'.format(lang), 'w') as outfile:
+            for line in infile:
+                newli = ['<s>', '<w>']
+                for w in line.split():
+                    newli.append(w)
+                    if w[-2:] != '@@':
+                        newli.append('<w>')
+                newli.extend(['</s>', '\n'])
+                outfile.write(' '.join(newli))
+
+def train_lm(lang):
+    vg = varikn.VarigramTrainer(False)
+    vg.set_datacost_scale2(0.002)
+    vg.set_max_order(20)
+    vg.initialize('filter_files/sents.seg.{}'.format(lang),
+        0, 0, -1, '', '<s>', False, '')
+    vg.grow(1)
+    vg.write_file('filter_files/lm.{}'.format(lang), False)
+
+def calculate_xent(lang):
+    lm = varikn.Perplexity(
+        'filter_files/lm.{}'.format(lang), 1, '', '', '', '<UNK>', 0, True)    
+    with open('filter_files/sents.bpe.{}'.format(lang)) as infile:
+        with open('filter_files/xent.{}'.format(lang), 'w') as xentfile:
+            for line in infile:
+                lpsum = 0.0
+                tokens = line.strip().split()
+                for token in tokens:
+                    lpsum += lm.word_logprob(token)
+                xent = -lpsum / lm.processed_words() / math.log10(2)
+                lm.clear_history()
+                lm.init_variables()
+                xentfile.write('{}\n'.format(str(xent)))
+
+segment(fromto[0])
+segment(fromto[1])
+
+train_lm(fromto[0])
+train_lm(fromto[1])
+
+calculate_xent(fromto[0])
+calculate_xent(fromto[1])
+
+subprocess.run('clean-corpus-n.perl filter_files/sents {0} {1} '
+    'filter_files/clean 1 50'.format(
+        fromto[0], fromto[1]).split())
+
+with open('filter_files/sents.{}'.format(fromto[0]), 'r') as pairs_file:
+    with open('filter_files/clean.{}'.format(fromto[0]), 'r') as clean_file:
+        with open('filter_files/clean_score.{0}-{1}'.format(
+                fromto[0], fromto[1]), 'w') as output_file:
+            for cline in clean_file:
+                while True:
+                    pline = pairs_file.readline()
+                    if pline == cline:
+                        output_file.write('1\n')
+                        break
+                    else:
+                        output_file.write('0\n')
+
+print('\n\nFilter scores are in "filter_files" directory in the following '
+    'files:\n\n'
+    'scores.{0}-{1}:\n'
+    '\tlangid_src\n'
+    '\tlangid_trg\n'
+    '\tcharacter_score_src\n'
+    '\tcharacter_score_trg\n'
+    '\tterminal_punctiation\n'
+    '\tnon_zero_numerals\n\n'
+    '{0}-{1}_score.fwd, '
+    '{0}-{1}_score.rev:\n'
+    '\tWord alignment\n\n'
+    'xent.{0}, '
+    'xent.{1}:\n'
+    '\tCross entropies\n\n'
+    'clean_score.{0}-{1}:\n'
+    '\tclean-corpus-n.perl\n'.format(fromto[0], fromto[1]))
+
