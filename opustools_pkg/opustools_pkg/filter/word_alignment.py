4cc74f18baae3a9d2d2e19f192407e1755048431
Sami Virpioja <sami.virpioja@helsinki.fi>
2019-09-03 11:53:56 +0300

    add methods for creating eflomal priors

--- opustools_pkg/opustools_pkg/filter/word_alignment.py
+++ opustools_pkg/opustools_pkg/filter/word_alignment.py
@@ -17,43 +17,52 @@ if EFLOMAL_PATH is None:
     EFLOMAL_PATH = '.'
 
 
-class WordAlignment:
-
-    def __init__(self, eflomal_path):
-        #self.eflomal_path = '/homeappl/home/miaulamo/appl_taito/eflomal'
-        self.eflomal_path = eflomal_path
-
-    def align(self, clean_file=None, src_fwd=None, trg_fwd=None,
-            src_score=None, trg_score=None, priors=None):
-        #command = ('{0}/align.py -i filter_files/clean.{1}-{2} -f '
-        #    'filter_files/{1}-{2}.fwd -r filter_files/{1}-{2}.rev '
-        #    '--model 3'.format(
-        #        self.eflomal_path, self.src_lang, self.trg_lang).split())
-        if src_score and trg_score:
-            command = ('{0}/align.py -i {1} -F {2} -R {3} '
-                '--model 3 -M 3 --priors {4}'.format(self.eflomal_path,
-                    clean_file, src_score, trg_score, priors).split())
-        else:
-            command = ('{0}/align.py -i {1} -f {2} -r {3} '
-                '--model 3'.format(self.eflomal_path, clean_file,
-                    src_fwd, trg_fwd).split())
-
-        subprocess.run(command)
-
-    def make_priors(self, clean_file=None, src_fwd=None,
-            trg_fwd=None, priors=None):
-        command = ('{0}/makepriors.py -i {1} -f {2} -r {3} '
-            '--priors {4}'.format(self.eflomal_path, clean_file,
-                src_fwd, trg_fwd, priors).split())
-        subprocess.run(command)
-
-
 def create_align_input_file(sentence_pairs):
     inputfile = tempfile.NamedTemporaryFile('w+')
     for sent1, sent2 in sentence_pairs:
         inputfile.write('{} ||| {}\n'.format(sent1, sent2))
     inputfile.flush()
     return inputfile
+
+
+def _run_eflomal_align(input_file, fwd_file, rev_file, model=3, priors=None):
+    """Run eflomal alignment and produce alignment files"""
+    priors_arg = '--priors {}'.format(priors) if priors else ''
+    command = '{path}/align.py --overwrite -i {input} -f {fwd} -r {rev} --model {model} -M {model} {priors}'.format(
+        path=EFLOMAL_PATH, input=input_file, fwd=fwd_file, rev=rev_file, model=model, priors=priors_arg)
+    return subprocess.run(command.split())
+
+
+def _run_eflomal_scoring(input_file, scores_fwd_file, scores_rev_file, model=3, priors=None):
+    """Run eflomal alignment and produce score files"""
+    priors_arg = '--priors {}'.format(priors) if priors else ''
+    command = '{path}/align.py -i {input} -F {fwd} -R {rev} --model {model} -M {model} {priors}'.format(
+        path=EFLOMAL_PATH, input=input_file, fwd=scores_fwd_file, rev=scores_rev_file,
+        model=model, priors=priors_arg)
+    return subprocess.run(command.split())
+
+
+def _run_eflomal_priors(input_file, scores_fwd_file, scores_rev_file, priors_file):
+    """Run eflomal prior estimation"""
+    command = '{path}/makepriors.py -i {input} -f {fwd} -r {rev} --priors {priors}'.format(
+        path=EFLOMAL_PATH, input=input_file, fwd=scores_fwd_file, rev=scores_rev_file,
+        priors=priors_file)
+    return subprocess.run(command.split())
+
+
+def make_priors(sentence_pairs, priors_file, model=3):
+    """Create alignment priors from clean sentence pairs"""
+    input_file = create_align_input_file(sentence_pairs)
+    fwd_file = tempfile.NamedTemporaryFile('w+')
+    rev_file = tempfile.NamedTemporaryFile('w+')
+    process = _run_eflomal_align(
+        input_file.name, fwd_file.name, rev_file.name, model=model, priors=None)
+    process.check_returncode()
+    process = _run_eflomal_priors(input_file.name, fwd_file.name, rev_file.name, priors_file)
+    process.check_returncode()
+    input_file.close()
+    fwd_file.close()
+    rev_file.close()
 
 
 class WordAlignFilter(FilterABC):
@@ -66,19 +75,13 @@ class WordAlignFilter(FilterABC):
         self.model = model
         super().__init__(**kwargs)
 
-    def _run(self, input_file, scores_fwd_file, scores_rev_file):
-        """Run eflomal alignment"""
-        priors_arg = '--priors {}'.format(self.priors) if self.priors else ''
-        command = '{path}/align.py -i {input} -F {fwd} -R {rev} --model {model} -M {model} {priors}'.format(
-            path=EFLOMAL_PATH, input=input_file, fwd=scores_fwd_file, rev=scores_rev_file,
-            model=self.model, priors=priors_arg)
-        subprocess.run(command.split())
-
     def score(self, pairs):
         input_file = create_align_input_file(pairs)
         scores_fwd_file = tempfile.NamedTemporaryFile('w+')
         scores_rev_file = tempfile.NamedTemporaryFile('w+')
-        self._run(input_file.name, scores_fwd_file.name, scores_rev_file.name)
+        process = _run_eflomal_scoring(input_file.name, scores_fwd_file.name, scores_rev_file.name,
+                                       model=self.model, priors=self.priors)
+        process.check_returncode()
         scores_fwd_file.seek(0)
         scores_rev_file.seek(0)
         for line1, line2 in zip(scores_fwd_file, scores_rev_file):
@@ -89,13 +92,15 @@ class WordAlignFilter(FilterABC):
 
     def accept(self, score):
         src, tgt = score
-        return src > self.src_threshold and tgt > self.tgt_threshold
+        return src < self.src_threshold and tgt < self.tgt_threshold
 
     def filter(self, pairs):
         input_file = create_align_input_file(pairs)
         scores_fwd_file = tempfile.NamedTemporaryFile('w+')
         scores_rev_file = tempfile.NamedTemporaryFile('w+')
-        self._run(input_file.name, scores_fwd_file.name, scores_rev_file.name)
+        process = _run_eflomal_scoring(input_file.name, scores_fwd_file.name, scores_rev_file.name,
+                                       model=self.model, priors=self.priors)
+        process.check_returncode()
         input_file.seek(0)
         scores_fwd_file.seek(0)
         scores_rev_file.seek(0)
