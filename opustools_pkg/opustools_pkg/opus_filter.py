6b2073317f5de26e6377de1655f979184334b8de
miau1 <aulamo.mikko@gmail.com>
2019-09-04 15:27:41 +0300

    opus_filter read yaml config files

--- opustools_pkg/opustools_pkg/opus_filter.py
+++ opustools_pkg/opustools_pkg/opus_filter.py
@@ -1,13 +1,15 @@
 import os
-import argparse
 import json
 import subprocess
 import logging
+import argparse
 
 from yaml import load, Loader
 
 from . import OpusRead
 from .filter.pipeline import FilterPipeline
+from .filter import lm
+from .filter import word_alignment
 
 
 class OpusFilter:
@@ -17,7 +19,9 @@ class OpusFilter:
         self.output_dir = configuration['output_directory']
         if not os.path.isdir(self.output_dir):
             logging.warning(
-                'Directory "{}" does not exist.'.format(self.output_dir))
+                'Directory "{}" does not exist. Writing files to current '
+                'directory.'.format(self.output_dir))
+            self.output_dir = '.'
 
         for corpus, settings in configuration['corpora'].items():
             if settings['type'] == 'OPUS':
@@ -29,54 +33,104 @@ class OpusFilter:
 
                 opus_reader = OpusRead('-d {corpus_name} -s {src} -t {tgt} '
                     '-r {version} -p {preprocessing} -wm moses '
-                    '-w {result_dir}/sents.{corpus_name}.{src} '
-                    '{result_dir}/sents.{corpus_name}.{tgt} -ln'.format(
+                    '-w {result_dir}/sents.{corpus}.{src} '
+                    '{result_dir}/sents.{corpus}.{tgt} -ln'.format(
                         corpus_name=parameters['corpus_name'], src=src_lan,
                         tgt=tgt_lan, version=parameters['release'],
                         preprocessing=parameters['preprocessing'],
-                        result_dir=self.output_dir).split())
+                        result_dir=self.output_dir, corpus=corpus).split())
 
                 opus_reader.printPairs()
 
+        for corpus, settings in configuration['scoring'].items():
+            for f in settings['filters']:
+                filter_name = next(iter(f.items()))[0]
+                if filter_name == 'WordAlignFilter':
+                    f[filter_name]['priors'] = '{}/{}'.format(
+                            self.output_dir, f[filter_name]['priors'])
+                if filter_name == 'CrossEntropyFilter':
+                    src_lm_params = f[filter_name]['src_lm_params']
+                    src_lm_params['filename'] = '{}/{}'.format(
+                            self.output_dir, src_lm_params['filename'])
+                    tgt_lm_params = f[filter_name]['tgt_lm_params']
+                    tgt_lm_params['filename'] = '{}/{}'.format(
+                            self.output_dir, tgt_lm_params['filename'])
+
+    def pair_generator(self, source_file_name, target_file_name):
+        with open(source_file_name) as source_file, \
+                open(target_file_name) as target_file:
+            for src_line in source_file:
+                tgt_line = target_file.readline()
+                yield (src_line.rstrip(), tgt_line.rstrip())
+
     def get_pairs(self, corpus_name, src, tgt):
-        source_file = open('{result_dir}/sents.{corpus_name}.{src}'.format(
-            result_dir=self.output_dir, corpus_name=corpus_name, src=src))
-        target_file = open('{result_dir}/sents.{corpus_name}.{tgt}'.format(
-            result_dir=self.output_dir, corpus_name=corpus_name, tgt=tgt))
-        for src_line in source_file:
-            tgt_line = target_file.readline()
-            yield (src_line.rstrip(), tgt_line.rstrip())
-        source_file.close()
-        target_file.close()
+        source_file_name = '{result_dir}/sents.{corpus_name}.{src}'.format(
+            result_dir=self.output_dir, corpus_name=corpus_name, src=src)
+        target_file_name = '{result_dir}/sents.{corpus_name}.{tgt}'.format(
+            result_dir=self.output_dir, corpus_name=corpus_name, tgt=tgt)
+
+        return self.pair_generator(source_file_name, target_file_name)
 
     def clean_data(self):
         for corpus, settings in self.configuration['filtering'].items():
-            clean_file= open('{result_dir}/{output_file}'.format(
-                result_dir=self.output_dir,
-                output_file=settings['output']), 'w')
 
             filter_pipe = FilterPipeline.from_config(settings['filters'])
             corpus_parameters = (self.configuration['corpora'][corpus]
                     ['parameters'])
-            corpus_name = corpus_parameters['corpus_name']
             source_language = corpus_parameters['source_language']
             target_language = corpus_parameters['target_language']
-            pairs_gen = self.get_pairs(corpus_name, source_language,
+            pairs_gen = self.get_pairs(corpus, source_language,
                     target_language)
             pairs = filter_pipe.filter(pairs_gen)
 
-            for pair in pairs:
-                clean_file.write('{} ||| {}\n'.format(pair[0], pair[1]))
-            clean_file.close()
+            source_file_name = '{result_dir}/{filtered_name}'.format(
+                result_dir=self.output_dir,
+                filtered_name=settings['src_output'])
+            target_file_name = '{result_dir}/{filtered_name}'.format(
+                result_dir=self.output_dir,
+                filtered_name=settings['tgt_output'])
+
+            with open(source_file_name, 'w') as source_file, \
+                    open(target_file_name, 'w') as target_file:
+                for pair in pairs:
+                    source_file.write(pair[0]+'\n')
+                    target_file.write(pair[1]+'\n')
+
+    def train_lms_and_priors(self):
+        for model in self.configuration['models']:
+            if model['type'] == 'ngram':
+                if model['parameters']['segmentation']['type'] == 'char':
+                    data_name = model['data']
+                    seg_name = data_name + '.seg'
+                    self.segment_file(
+                        '{}/{}'.format(self.output_dir, data_name),
+                        '{}/{}'.format(self.output_dir, seg_name))
+                    model['data'] = seg_name
+
+                train_args = argparse.Namespace()
+                for key, default in lm._VARIKN_TRAINING_PARAMS.items():
+                    setattr(train_args, key, default)
+                for key, value in model.items():
+                    if key in ['data', 'output', 'model']:
+                        value = '{}/{}'.format(self.output_dir, value)
+                    setattr(train_args, key, value)
+                lm.train(train_args)
+            if model['type'] == 'alignment':
+                pair_gen = self.pair_generator(
+                        '{}/{}'.format(self.output_dir, model['src_data']),
+                        '{}/{}'.format(self.output_dir, model['tgt_data']))
+                word_alignment.make_priors(
+                        pair_gen,
+                        '{}/{}'.format(self.output_dir, model['output']),
+                        model=model['parameters']['model'])
 
     def score_data(self):
-        for corpus, settings in self.configuration['filtering'].items():
+        for corpus, settings in self.configuration['scoring'].items():
             corpus_parameters = (self.configuration['corpora'][corpus]
                     ['parameters'])
-            corpus_name = corpus_parameters['corpus_name']
             source_language = corpus_parameters['source_language']
             target_language = corpus_parameters['target_language']
-            pairs_gen = self.get_pairs(corpus_name, source_language,
+            pairs_gen = self.get_pairs(corpus, source_language,
                     target_language)
 
             filter_pipe = FilterPipeline.from_config(settings['filters'])
@@ -85,7 +139,7 @@ class OpusFilter:
 
             score_file = open(
                 '{result_dir}/scores.{corpus_name}.{src}-{tgt}.json'.format(
-                    result_dir=self.output_dir, corpus_name=corpus_name,
+                    result_dir=self.output_dir, corpus_name=corpus,
                     src=source_language, tgt=target_language) , 'w')
             score_file.write(json.dumps(scores))
             score_file.close()
@@ -110,7 +164,7 @@ class OpusFilter:
         return '<s> <w> {} <w> </s> \n'.format(
                 ' '.join(line.rstrip()).replace('   ', ' <w> '))
 
-    def segment_file(self, input_file, output_file, char=False):
+    def segment_file(self, input_file, output_file, char=True):
         with open(input_file, 'r') as infile:
             with open(output_file, 'w') as outfile:
                 for line in infile:
--- opustools_pkg/tests/test_opus_filter.py
+++ opustools_pkg/tests/test_opus_filter.py
@@ -9,41 +9,70 @@ class TestOpusFilter(unittest.TestCase):
 
     def setUp(self):
         configuration = {'output_directory': 'filter_files',
-                'corpora': {
-                    'RF': {
-                        'type': 'OPUS',
-                        'parameters': {
-                            'corpus_name': 'RF',
-                            'source_language': 'en',
-                            'target_language': 'sv',
-                            'release': 'latest',
-                            'preprocessing': 'xml'
-                            }
-                        }
-                    },
-                'filtering': {
-                    'RF': {
-                        'output': 'RF_filtered.en-sv',
-                        'filters': []
-                        }
-                    }
-                }
+            'corpora': {'RF1': {'type': 'OPUS',
+              'parameters': {'corpus_name': 'RF',
+               'source_language': 'en',
+               'target_language': 'sv',
+               'release': 'latest',
+               'preprocessing': 'xml'}}},
+            'filtering': {'RF1': {'src_output': 'RF1_filtered.en',
+              'tgt_output': 'RF1_filtered.sv',
+              'filters': [{'LanguageIDFilter': {'src_lang': 'en',
+                 'tgt_lang': 'sv',
+                 'src_threshold': 0,
+                 'tgt_threshold': 0}},
+               {'TerminalPunctuationFilter': {'threshold': -2}},
+               {'NonZeroNumeralsFilter': {'threshold': 0.5}},
+               {'CharacterScoreFilter': {'src_script': 'latin-1',
+                 'tgt_script': 'latin-1',
+                 'src_threshold': 1,
+                 'tgt_threshold': 1}}]}},
+            'models': [{'type': 'ngram',
+              'data': 'RF1_filtered.en',
+              'language': 'en',
+              'parameters': {'segmentation': {'type': 'char'},
+               'norder': 20,
+               'dscale': 0.001},
+              'model': 'RF1_en.arpa'},
+             {'type': 'ngram',
+              'data': 'RF1_filtered.sv',
+              'language': 'sv',
+              'parameters': {'segmentation': {'type': 'char'},
+               'norder': 20,
+               'dscale': 0.001},
+              'model': 'RF1_sv.arpa'},
+             {'type': 'alignment',
+              'src_data': 'RF1_filtered.en',
+              'tgt_data': 'RF1_filtered.sv',
+              'parameters': {'model': 3},
+              'output': 'RF1_align.priors'}],
+            'scoring': {'RF1': {'output': 'RF1_scores.en-sv.json',
+              'filters': [{'LanguageIDFilter': {'src_lang': 'en',
+                 'tgt_lang': 'sv',
+                 'src_threshold': 0,
+                 'tgt_threshold': 0}},
+               {'TerminalPunctuationFilter': {'threshold': -2}},
+               {'NonZeroNumeralsFilter': {'threshold': 0.5}},
+               {'CharacterScoreFilter': {'src_script': 'latin-1',
+                 'tgt_script': 'latin-1',
+                 'src_threshold': 1,
+                 'tgt_threshold': 1}},
+               {'WordAlignFilter': {'priors': 'RF1_align.priors',
+                 'model': 3,
+                 'src_threshold': 0,
+                 'tgt_threshold': 0}},
+               {'CrossEntropyFilter': {'src_lm_params': {'segmentation': {'type': 'char'},
+                  'filename': 'RF1_en.arpa'},
+                 'tgt_lm_params': {'segmentation': {'type': 'char'},
+                  'filename': 'RF1_sv.arpa'},
+                 'src_threshold': 50.0,
+                 'tgt_threshold': 50.0,
+                 'diff_threshold': 10.0}}]}}}
+
         self.opus_filter = OpusFilter(configuration)
 
-        #self.opus_filter.sents_to_file()
-        self.opus_filter.segment_file(
-                'filter_files/sents.RF.en',
-                'filter_files/sents.seg.RF.en',
-                char=True
-                )
-        self.opus_filter.segment_file(
-                'filter_files/sents.RF.sv',
-                'filter_files/sents.seg.RF.sv',
-                char=True
-                )
-
     def test_get_pairs(self):
-        pair_gen = self.opus_filter.get_pairs('RF', 'en', 'sv')
+        pair_gen = self.opus_filter.get_pairs('RF1', 'en', 'sv')
         pair = next(pair_gen)
         for pair in pair_gen:
             pass
@@ -52,66 +81,41 @@ class TestOpusFilter(unittest.TestCase):
                 'Så kan vi hålla samman Sverige .'))
 
     def test_clean_data(self):
-        self.opus_filter.configuration['filtering']['RF']['filters'] = [
-                {'LengthFilter': {'min_length': 5, 'max_length': 5}},
-                {'LengthRatioFilter': {'threshold': 1.1}},
-                {'LongWordFilter': {}},
-                {'HtmlTagFilter': {}},
-                {'CharacterScoreFilter': {}}
-           ]
-
         self.opus_filter.clean_data()
 
-        with open('filter_files/RF_filtered.en-sv') as clean:
+        with open('filter_files/RF1_filtered.en') as clean:
             self.assertEqual(
-                    clean.readlines(),
-                    ['Welfare presupposes fair distribution . ||| '
-                    'Kommunerna får stabila spelregler .\n']
+                    clean.readline(),
+                    'Your Majesties , Your Royal Highnesses , Mr Speaker , '
+                    'Members of the Swedish Parliament .\n'
+                    )
+        with open('filter_files/RF1_filtered.sv') as clean:
+            self.assertEqual(
+                    clean.readline(),
+                    'Eders Majestäter , Eders Kungliga Högheter , herr '
+                    'talman , ledamöter av Sveriges riksdag !\n'
                     )
 
     def test_score_data(self):
-        self.opus_filter.configuration['filtering']['RF']['filters'] = [
-                {'LanguageIDFilter': {'src_lang': 'en', 'tgt_lang': 'sv'}},
-                {'CharacterScoreFilter': {}},
-                {'TerminalPunctuationFilter': {}},
-                {'NonZeroNumeralsFilter': {}},
-                {'CrossEntropyFilter':
-                    {'src_lm_params':
-                        {'filename': 'filter_files/lm.en'},
-                    'tgt_lm_params':
-                        {'filename': 'filter_files/lm.sv'}
-                    }},
-                {'WordAlignFilter': {}}
-           ]
-
-        train_args = argparse.Namespace()
-        for key, default in lm._VARIKN_TRAINING_PARAMS.items():
-            setattr(train_args, key, default)
-        train_args.data = 'filter_files/sents.seg.RF.en'
-        train_args.model = 'filter_files/lm.en'
-        lm.train(train_args)
-        train_args.data = 'filter_files/sents.seg.RF.sv'
-        train_args.model = 'filter_files/lm.sv'
-        lm.train(train_args)
-
+        self.opus_filter.train_lms_and_priors()
         self.opus_filter.score_data()
 
-        with open('filter_files/scores.RF.en-sv.json') as scores_file:
+        with open('filter_files/scores.RF1.en-sv.json') as scores_file:
             scores = json.loads(scores_file.readline())
             self.assertEqual(scores[0]['LanguageIDFilter'], [1.0, 0.98])
             self.assertEqual(scores[0]['CharacterScoreFilter'], [1.0, 1.0])
             self.assertEqual(scores[0]['CrossEntropyFilter'],
-                    [11.311833035785716, 25.531239597436915])
+                    [15.214258903317491, 7.569084909162213])
             self.assertEqual(scores[0]['TerminalPunctuationFilter'], -0.0)
             self.assertEqual(scores[0]['NonZeroNumeralsFilter'], 0.0)
             self.assertEqual(type(scores[0]['WordAlignFilter']), list)
 
     def test_make_bpe(self):
-        train_file = 'filter_files/sents.RF.en'
-        input_file = 'filter_files/sents.RF.en'
-        output_file = 'filter_files/sents.RF.en.bpe'
+        train_file = 'filter_files/sents.RF1.en'
+        input_file = 'filter_files/sents.RF1.en'
+        output_file = 'filter_files/sents.RF1.en.bpe'
         self.opus_filter.make_bpe(train_file, input_file, output_file)
-        with open('filter_files/sents.RF.en.bpe') as bped_file:
+        with open('filter_files/sents.RF1.en.bpe') as bped_file:
             bped_lines = bped_file.readlines()
             self.assertEqual(len(bped_lines), 180)
             self.assertEqual(
@@ -160,15 +164,16 @@ class TestOpusFilter(unittest.TestCase):
                 '<s> <w> t h i s <w> i s <w> a <w> t e s t <w> </s> \n')
 
     def test_segment_file(self):
-        train_file = 'filter_files/sents.RF.en'
-        input_file = 'filter_files/sents.RF.en'
-        output_file = 'filter_files/sents.RF.en.bpe'
+        train_file = 'filter_files/sents.RF1.en'
+        input_file = 'filter_files/sents.RF1.en'
+        output_file = 'filter_files/sents.RF1.en.bpe'
         self.opus_filter.make_bpe(train_file, input_file, output_file)
         self.opus_filter.segment_file(
-                'filter_files/sents.RF.en.bpe',
-                'filter_files/sents.RF.en.bpe.seg'
+                'filter_files/sents.RF1.en.bpe',
+                'filter_files/sents.RF1.en.bpe.seg',
+                char=False
                 )
-        with open('filter_files/sents.RF.en.bpe.seg') as output_file:
+        with open('filter_files/sents.RF1.en.bpe.seg') as output_file:
             seg_lines = output_file.readlines()
             self.assertEqual(len(seg_lines), 180)
             self.assertEqual(
@@ -182,10 +187,11 @@ class TestOpusFilter(unittest.TestCase):
                     '198@@ 8 <w> . <w> </s> \n')
                     )
         self.opus_filter.segment_file(
-                'filter_files/sents.RF.en',
-                'filter_files/sents.RF.en.seg'
+                'filter_files/sents.RF1.en',
+                'filter_files/sents.RF1.en.seg',
+                char=False
                 )
-        with open('filter_files/sents.RF.en.seg') as output_file:
+        with open('filter_files/sents.RF1.en.seg') as output_file:
             seg_lines = output_file.readlines()
             self.assertEqual(len(seg_lines), 180)
             self.assertEqual(
@@ -199,11 +205,10 @@ class TestOpusFilter(unittest.TestCase):
 
     def test_segment_file_char(self):
         self.opus_filter.segment_file(
-                'filter_files/sents.RF.en',
-                'filter_files/sents.RF.en.seg',
-                char=True
+                'filter_files/sents.RF1.en',
+                'filter_files/sents.RF1.en.seg'
                 )
-        with open('filter_files/sents.RF.en.seg') as output_file:
+        with open('filter_files/sents.RF1.en.seg') as output_file:
             seg_lines = output_file.readlines()
             self.assertEqual(len(seg_lines), 180)
             self.assertEqual(
@@ -217,9 +222,9 @@ class TestOpusFilter(unittest.TestCase):
                     '<w> , <w> 1 9 8 8 <w> . <w> </s> \n')
                     )
 
-    def test_sents_to_file(self):
-        with open('filter_files/sents.RF.en') as sents_file_en:
-            with open('filter_files/sents.RF.sv') as sents_file_sv:
+    def test_initial_files(self):
+        with open('filter_files/sents.RF1.en') as sents_file_en:
+            with open('filter_files/sents.RF1.sv') as sents_file_sv:
                 sents_en = sents_file_en.readlines()
                 sents_sv = sents_file_sv.readlines()
                 self.assertEqual(len(sents_en), 180)
