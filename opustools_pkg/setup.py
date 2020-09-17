28c32b95f3c09230119132aea340339601df7a20
miau1 <aulamo.mikko@gmail.com>
2020-09-17 15:10:10 +0300

    included an init file in parse

--- opustools_pkg/setup.py
+++ opustools_pkg/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="opustools",
-    version="0.0.54",
+    version="1.0.0",
     author="Mikko Aulamo",
     author_email="mikko.aulamo@helsinki.fi",
     description="Tools to read OPUS",
