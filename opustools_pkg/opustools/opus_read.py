5ed5acc82e93c3943e0e67fb0481f42e7df77656
Sami Virpioja <sami.virpioja@helsinki.fi>
2024-08-07 16:46:25 +0300

    fix moses output formats, error printing etc.

--- opustools_pkg/opustools/opus_read.py
+++ opustools_pkg/opustools/opus_read.py
@@ -1,6 +1,8 @@
+import logging
 import os
-import shutil
 import re
+import sys
+import tempfile
 
 from .parse.alignment_parser import AlignmentParser
 from .parse.sentence_parser import SentenceParser, SentenceParserError
@@ -8,13 +10,19 @@ from .util import file_open
 from .formatting import *
 from .opus_file_handler import OpusFileHandler
 
+
+logger = logging.getLogger(__name__)
+
+
 def skip_regex_type(n, N):
     "Select function to skip document names"
 
     def get_re(doc_name):
         return not re.search(n, doc_name)
+
     def skip_re(doc_name):
         return re.search(N, doc_name)
+
     def nothing(doc_name):
         return False
 
@@ -27,7 +35,8 @@ def skip_regex_type(n, N):
 
 class OpusRead:
 
-    def __init__(self, directory=None, source=None, target=None,
+    def __init__(
+            self, directory=None, source=None, target=None,
             release='latest', preprocess='xml', maximum=-1, src_range='all',
             tgt_range='all', attribute=None, threshold=None,
             leave_non_alignments_out=False, write=None, write_mode='normal',
@@ -39,7 +48,8 @@ class OpusRead:
             change_annotation_delimiter='|',
             src_cld2=None, trg_cld2=None, src_langid=None, trg_langid=None,
             write_ids=None, suppress_prompts=False, download_dir='.',
-            preserve_inline_tags=False, n=None, N=None, chunk_size=1000000, verbose=False):
+            preserve_inline_tags=False, n=None, N=None, chunk_size=1000000,
+            verbose=False):
         """Read xces alignment files and xml sentence files and output in
         desired format.
 
@@ -83,7 +93,8 @@ class OpusRead:
         preserve_inline_tags -- Preserve inline tags within sentences
         n -- Get only documents that match the regex
         N -- Skip all doucments that match the regex
-        chunk_size -- Number of sentence pairs in chunks to be processed (default 1000000)
+        chunk_size -- Number of sentence pairs in chunks to be processed
+            (default 1000000)
         verbose -- Print progress messages
         """
 
@@ -117,28 +128,32 @@ class OpusRead:
 
         lang_filters = [src_cld2, src_langid, trg_cld2, trg_langid]
 
-        default_alignment = os.path.join(root_directory, directory, release,
-                'xml', self.fromto[0]+'-'+self.fromto[1]+'.xml.gz')
+        default_alignment = os.path.join(
+            root_directory, directory, release, 'xml',
+            self.fromto[0]+'-'+self.fromto[1]+'.xml.gz')
         if alignment_file == -1:
             self.alignment = default_alignment
         else:
             self.alignment = alignment_file
 
+        dl_prefix = directory + '_' + release + '_' + preprocess + '_'
         if not source_zip:
-            dl_src_zip = os.path.join(download_dir, directory+'_'+release+'_'+
-                preprocess+'_'+self.fromto[0]+'.zip')
+            dl_src_zip = os.path.join(
+                download_dir, dl_prefix + self.fromto[0] + '.zip')
             if os.path.isfile(dl_src_zip):
                 source_zip = dl_src_zip
             else:
-                source_zip = os.path.join(root_directory, directory, release,
+                source_zip = os.path.join(
+                    root_directory, directory, release,
                     preprocess, self.fromto[0]+'.zip')
         if not target_zip:
-            dl_trg_zip = os.path.join(download_dir, directory+'_'+release+'_'+
-                preprocess+'_'+self.fromto[1]+'.zip')
+            dl_trg_zip = os.path.join(
+                download_dir, dl_prefix + self.fromto[1] + '.zip')
             if os.path.isfile(dl_trg_zip):
                 target_zip = dl_trg_zip
             else:
-                target_zip = os.path.join(root_directory, directory, release,
+                target_zip = os.path.join(
+                    root_directory, directory, release,
                     preprocess, self.fromto[1]+'.zip')
 
         if write_ids:
@@ -187,19 +202,23 @@ class OpusRead:
                 preprocess, self.fromto, suppress_prompts)
 
         if preprocess == 'moses':
+            if self.write_mode != 'moses':
+                logger.warning("Only moses write_mode is supported for moses preprocessing. "
+                               "Ignoring write_mode %s.", self.write_mode)
+                self.write_mode = 'moses'
             return
 
         store_attrs = False
-        if write_mode == "links" or write_ids != None:
+        if write_mode == "links" or write_ids is not None:
             store_attrs = True
 
         self.alignment = self.of_handler.open_alignment_file(self.alignment)
-        self.alignmentParser = AlignmentParser(self.alignment,
-                (src_range, tgt_range), attribute, threshold, store_attrs,
-                leave_non_alignments_out)
+        self.alignmentParser = AlignmentParser(
+            self.alignment, (src_range, tgt_range), attribute, threshold,
+            store_attrs, leave_non_alignments_out)
 
     def printPairs(self):
-        print("printPairs called!")
+        logger.debug("printPairs called!")
         resultfile = None
         mosessrc = None
         mosestrg = None
@@ -217,19 +236,29 @@ class OpusRead:
 
         if self.preprocess == 'moses':
             # If preprocessing is moses, download
-            moses_names = self.of_handler.open_moses_files()
-            if self.write:
-                if len(self.write) == 2:
-                    if not self.switch_langs:
-                        shutil.move(moses_names[0], self.write[0])
-                        shutil.move(moses_names[1], self.write[1])
-                    else:
-                        shutil.move(moses_names[0], self.write[1])
-                        shutil.move(moses_names[1], self.write[0])
-                    moses_names = self.write
-                else:
-                    print('"moses" preprocessing requires two output file names. Using default names.')
-            print(f'Moses files written to {", ".join(moses_names)}')
+            if not self.write or len(self.write) != 2:
+                # Write to current path and return
+                if self.write and len(self.write) != 2:
+                    logger.warning('"moses" preprocessing requires two output '
+                                   'file names. Using default names.')
+                moses_names = self.of_handler.open_moses_files(
+                    outpath=self.of_handler.download_dir)
+                logger.info('Moses files written to %s', ', '.join(moses_names))
+                return
+            with tempfile.TemporaryDirectory() as tmpdir:
+                # Write to specified files
+                logger.info('Extracting data...')
+                moses_names = self.of_handler.open_moses_files(outpath=tmpdir)
+                with file_open(os.path.join(tmpdir, moses_names[0])) as in1, \
+                     file_open(os.path.join(tmpdir, moses_names[1])) as in2:
+                    if self.switch_langs:
+                        in1, in2 = in2, in1
+                    for fin, fout in [(in1, mosessrc), (in2, mosestrg)]:
+                        for line in fin:
+                            fout.write(line)
+                mosessrc.close()
+                mosestrg.close()
+                logger.info('Moses files written to %s', ', '.join(self.write))
             return
 
         self.add_file_header(resultfile)
@@ -251,13 +280,14 @@ class OpusRead:
                 self.alignmentParser.collect_links(cur_pos, self.chunk_size, self.verbose)
 
             if src_doc_name != prev_src_doc_name:
-               src_doc_size = -1
+                src_doc_size = -1
             prev_src_doc_name = src_doc_name
             if trg_doc_name != prev_trg_doc_name:
-               trg_doc_size = -1
+                trg_doc_size = -1
             prev_trg_doc_name = trg_doc_name
 
-            if self.verbose: print("")
+            if self.verbose:
+                print("", file=sys.stderr)
 
             if not src_doc_name:
                 break
@@ -271,32 +301,32 @@ class OpusRead:
                     src_doc = self.of_handler.open_sentence_file(src_doc_name, 'src')
                     trg_doc = self.of_handler.open_sentence_file(trg_doc_name, 'trg')
                 except KeyError as e:
-                    print('\n'+e.args[0]+'\nContinuing from next sentence file pair.')
+                    print('\n'+e.args[0]+'\nContinuing from next sentence file pair.', file=sys.stderr)
                     continue
 
                 try:
-                    src_parser = SentenceParser(src_doc,
-                            preprocessing=self.preprocess, anno_attrs=self.src_annot,
-                            preserve=self.preserve, delimiter=self.annot_delimiter)
+                    src_parser = SentenceParser(
+                        src_doc, preprocessing=self.preprocess, anno_attrs=self.src_annot,
+                        preserve=self.preserve, delimiter=self.annot_delimiter)
                     src_doc_size = src_parser.store_sentences(src_set, src_doc_size, self.verbose)
-                    trg_parser = SentenceParser(trg_doc,
-                            preprocessing=self.preprocess, anno_attrs=self.trg_annot,
-                            preserve=self.preserve, delimiter=self.annot_delimiter)
+                    trg_parser = SentenceParser(
+                        trg_doc, preprocessing=self.preprocess, anno_attrs=self.trg_annot,
+                        preserve=self.preserve, delimiter=self.annot_delimiter)
                     trg_doc_size = trg_parser.store_sentences(trg_set, trg_doc_size, self.verbose)
                 except SentenceParserError as e:
-                    print('\n'+e.message+'\nContinuing from next sentence file pair.')
+                    print('\n'+e.message+'\nContinuing from next sentence file pair.', file=sys.stderr)
                     continue
 
-            self.add_doc_names(src_doc_name, trg_doc_name,
-                    resultfile, mosessrc, mosestrg)
+            self.add_doc_names(
+                src_doc_name, trg_doc_name, resultfile, mosessrc, mosestrg)
 
             len_link_list = len(link_list)
 
             for i, link_a in enumerate(link_list):
                 if self.verbose:
-                    if i%1000==0 or i+1==len_link_list:
+                    if i % 1000 == 0 or i + 1 == len_link_list:
                         progress = str(round((i+1)/len_link_list*100, 2))
-                        print("\x1b[2KWriting chunk ... {}%".format(progress), end="\r")
+                        print("\x1b[2KWriting chunk ... {}%".format(progress), end="\r", file=sys.stderr)
 
                 src_result, trg_result = self.format_pair(
                         link_a, src_parser, trg_parser, self.fromto)
@@ -306,9 +336,9 @@ class OpusRead:
 
                 link_attr = attrs_list[i] if i < len(attrs_list) else None
 
-                self.out_put_pair(src_result, trg_result, resultfile,
-                        mosessrc, mosestrg, link_attr, id_file,
-                        src_doc_name, trg_doc_name)
+                self.out_put_pair(
+                    src_result, trg_result, resultfile, mosessrc, mosestrg,
+                    link_attr, id_file, src_doc_name, trg_doc_name)
 
                 total += 1
                 if total == self.maximum:
@@ -318,13 +348,13 @@ class OpusRead:
             self.add_doc_ending(resultfile)
 
             if self.verbose and self.write:
-                print("\033[F\033[F\033[F", end="")
+                print("\033[F\033[F\033[F", end="", file=sys.stderr)
 
             if stop:
                 break
 
         if self.verbose and self.write:
-            print("\n\n")
+            print("\n\n", file=sys.stderr)
 
         self.add_file_ending(resultfile)
 
--- opustools_pkg/opustools/readopusdata.py
+++ opustools_pkg/opustools/readopusdata.py
@@ -5,13 +5,19 @@ import os
 
 from ruamel.yaml import YAML, scanner, reader
 
+
+logger = logging.getLogger(__name__)
+
+
 def read_url(url):
     return urllib.request.urlopen(url).read().decode('utf-8').split('\n')
+
 
 def read_url_yaml(url, yaml):
     raw = urllib.request.urlopen(url).read().decode('utf-8')
     data = yaml.load(raw)
     return data
+
 
 def create_table(cur):
     create_opusfile_table = '''CREATE TABLE IF NOT EXISTS opusfile (
@@ -34,6 +40,7 @@ def create_table(cur):
     create_url_index = 'CREATE INDEX IF NOT EXISTS idx_url ON opusfile(url)'
     cur.execute(create_url_index)
 
+
 def execute_sql(cur, opusfile):
     columns = ['source', 'target', 'corpus', 'preprocessing', 'version', 'url', 'size', 'documents', 'alignment_pairs', 'source_tokens', 'target_tokens', 'latest']
     #wheres = [f'{columns[i]}="{opusfile[i]}"' for i in range(6)]
@@ -48,71 +55,73 @@ def execute_sql(cur, opusfile):
         sql = f'INSERT INTO opusfile({", ".join(columns)}, updated) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,1)'
         cur.execute(sql, opusfile)
 
+
 def get_lang_info(name, data, data_type, info):
     source, target, documents, alignment_pairs, source_tokens, target_tokens = '', '', '', '', '', ''
     source = name
     if data_type in ['bitexts', 'moses', 'tmx']:
         names = name.split('-')
         if len(names) != 2:
-            logging.warning(f'{info} {data_type} {name}: cannot split name "{name}" into two language codes')
+            logger.warning(f'{info} {data_type} {name}: cannot split name "{name}" into two language codes')
         else:
             source, target = names
     documents = ''
     if data_type in ['bitexts', 'monolingual']:
         documents = data.get('files', '')
         if documents == '':
-            logging.warning(f'{info} {data_type} {name} is missing "files"')
+            logger.warning(f'{info} {data_type} {name} is missing "files"')
     if data_type in ['bitexts', 'moses']:
         alignment_pairs = data.get('alignments', '')
         if alignment_pairs == '':
-            logging.warning(f'{info} {data_type} {name} is missing "alignments"')
+            logger.warning(f'{info} {data_type} {name} is missing "alignments"')
     elif data_type == 'tmx':
         alignment_pairs = data.get('translation units', '')
         if alignment_pairs == '':
-            logging.warning(f'{info} {data_type} {name} is missing "translation units"')
+            logger.warning(f'{info} {data_type} {name} is missing "translation units"')
     elif data_type == 'monolingual':
         alignment_pairs = data.get('sentences', '')
         if alignment_pairs == '':
-            logging.warning(f'{info} {data_type} {name} is missing "sentences"')
+            logger.warning(f'{info} {data_type} {name} is missing "sentences"')
     if data_type == 'monolingual':
         source_tokens = data.get('tokens', '')
         if source_tokens == '':
-            logging.warning(f'{info} {data_type} {name} is missing "tokens"')
+            logger.warning(f'{info} {data_type} {name} is missing "tokens"')
         target_tokens = ''
     else:
         source_tokens = data.get('source language tokens', '')
         if source_tokens == '':
-            logging.warning(f'{info} {data_type} {name} is missing "source language tokens"')
+            logger.warning(f'{info} {data_type} {name} is missing "source language tokens"')
         target_tokens = data.get('target language tokens', '')
         if target_tokens == '':
-            logging.warning(f'{info} {data_type} {name} is missing "target language tokens"')
+            logger.warning(f'{info} {data_type} {name} is missing "target language tokens"')
 
     return source, target, documents, alignment_pairs, source_tokens, target_tokens
+
 
 def get_size_url_prep(name, data, data_type, info):
     size, url, preprocessing = '','',''
     if data_type in ['tmx', 'moses']:
         size = data.get('download size', '')
         if size == '':
-            logging.warning(f'{info} {data_type} {name} is missing "download size"')
+            logger.warning(f'{info} {data_type} {name} is missing "download size"')
         else:
             size = int(int(size)/1024)
         url = data.get('download url', '')
         if url == '':
-            logging.warning(f'{info} {data_type} {name} is missing "download url"')
+            logger.warning(f'{info} {data_type} {name} is missing "download url"')
     elif data_type in ['bitexts', 'monolingual']:
         size = data.get('size', '')
         if size == '':
-            logging.warning(f'{info} {data_type} {name} is missing "size"')
+            logger.warning(f'{info} {data_type} {name} is missing "size"')
         else:
             size = int(int(size)/1024)
         url = data.get('url', '')
         if url == '':
-            logging.warning(f'{info} {data_type} {name} is missing "url"')
+            logger.warning(f'{info} {data_type} {name} is missing "url"')
 
     pre_step = url.split('/')
     if len(pre_step) < 2:
-        logging.warning(f'{info} {data_type} {name}: cannot find preprocessing from url "{url}"')
+        logger.warning(f'{info} {data_type} {name}: cannot find preprocessing from url "{url}"')
     else:
         preprocessing = pre_step[-2]
 
@@ -125,12 +134,14 @@ def get_tmx_entries(corpus, version, latest, tmx, cur, info):
         opusfile = (source, target, corpus, preprocessing, version, url, size, documents, alignment_pairs, source_tokens, target_tokens, latest)
         execute_sql(cur, opusfile)
 
+
 def get_moses_entries(corpus, version, latest, moses, cur, info):
     for item in moses:
         source, target, documents, alignment_pairs, source_tokens, target_tokens = get_lang_info(item, moses[item], 'moses', info)
         size, url, preprocessing = get_size_url_prep(item, moses[item], 'moses', info)
         opusfile = (source, target, corpus, preprocessing, version, url, size, documents, alignment_pairs, source_tokens, target_tokens, latest)
         execute_sql(cur, opusfile)
+
 
 def get_monolingual_entries(corpus, version, latest, monolingual, cur, info):
     for item in monolingual:
@@ -139,6 +150,7 @@ def get_monolingual_entries(corpus, version, latest, monolingual, cur, info):
             size, url, preprocessing = get_size_url_prep(item, entry[1], 'monolingual', info)
             opusfile = (source, target, corpus, preprocessing, version, url, size, documents, alignment_pairs, source_tokens, target_tokens, latest)
             execute_sql(cur, opusfile)
+
 
 def get_bitext_entries(corpus, version, latest, bitexts, cur, info):
     for item in bitexts:
@@ -150,11 +162,13 @@ def get_bitext_entries(corpus, version, latest, bitexts, cur, info):
                 opusfile = (source, target, corpus, preprocessing, version, url, size, documents, alignment_pairs, source_tokens, target_tokens, latest)
                 execute_sql(cur, opusfile)
 
+
 def remove_missing_items(cur):
     sql = 'DELETE FROM opusfile WHERE updated=0'
     cur.execute(sql)
     sql = 'UPDATE opusfile SET updated=0'
     cur.execute(sql)
+
 
 def update_db(db_file=None, log_type='errors'):
     yaml = YAML()
@@ -183,15 +197,15 @@ def update_db(db_file=None, log_type='errors'):
             try:
                 gen_info = read_url_yaml(URL_BASE + info, yaml)
             except (scanner.ScannerError, urllib.error.HTTPError, reader.ReaderError) as e:
-                logging.error(f'{info}, {type(e).__name__}: {e}')
+                logger.error(f'{info}, {type(e).__name__}: {e}')
                 gen_info = {}
             corpus = gen_info.get('name')
             if not corpus:
-                logging.warning(f'{info}, corpus name missing')
+                logger.warning(f'{info}, corpus name missing')
             print(f'Processing corpus {corpus}')
             latest_v = gen_info.get('latest_release')
             if not latest_v:
-                logging.error(f'{info}, latest_release missing')
+                logger.error(f'{info}, latest_release missing')
         elif len(info_s) == 3:
             version = info_s[1]
             if not corpus:
@@ -203,7 +217,7 @@ def update_db(db_file=None, log_type='errors'):
             try:
                 corpus_data = read_url_yaml(URL_BASE + stats, yaml)
             except (scanner.ScannerError, urllib.error.HTTPError, reader.ReaderError) as e:
-                logging.error(f'{stats}, {type(e).__name__}: {e}')
+                logger.error(f'{stats}, {type(e).__name__}: {e}')
                 continue
 
             get_entries = {'bitexts': get_bitext_entries,
@@ -212,7 +226,7 @@ def update_db(db_file=None, log_type='errors'):
                             'tmx': get_tmx_entries}
 
             if not corpus_data:
-                logging.error(f'{info}, corpus_data is empty')
+                logger.error(f'{info}, corpus_data is empty')
                 continue
 
             for item in get_entries.keys():
@@ -220,15 +234,17 @@ def update_db(db_file=None, log_type='errors'):
                 if sub_data:
                     get_entries[item](corpus, version, latest, sub_data, cur, info)
                 else:
-                    logging.warning(f'{info}, {item} data missing')
+                    logger.warning(f'{info}, {item} data missing')
 
     remove_missing_items(cur)
 
     con.commit()
     con.close()
 
+
 def main():
     update_db()
+
 
 if __name__ == "__main__":
     main()
